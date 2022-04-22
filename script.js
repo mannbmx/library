814d0385b628c24ed159dfa4dbecba1970de06f7
mannbmx <mannbmx@gmail.com>
2022-04-22 13:34:25 -0400

    Added book creation and styling

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let library = [];
 
+
+//Both creates a book and adds it to the DOM for display
 function addBook(name, author, pages){
     this.name = name;
     this.author = author;
     this.pages = pages;
+    addToLibrary(this);
+
+    let libCont = document.querySelector('.content');
+    let book1 = document.createElement('div');
+    book1.className = 'bookCard';
+    libCont.appendChild(book1);
+
+    let bookTitle = document.createElement('h4');
+    bookTitle.textContent ='Title: ' + this.name;
+    bookTitle.classList = 'bookInfo title'
+    book1.appendChild(bookTitle);
+
+    let bookAuthor = document.createElement('h4');
+    bookAuthor.textContent ='Author: ' + this.author;
+    bookAuthor.classList = 'bookInfo author'
+    book1.appendChild(bookAuthor);
+
+    let bookPages = document.createElement('h4');
+    bookPages.textContent = 'Pages: ' + this.pages;
+    bookPages.classList = 'bookInfo pages'
+    book1.appendChild(bookPages);
 };
 
 
@@ -12,26 +35,19 @@ function addToLibrary(newBook){
     library.push(newBook);
 }
 
-// For loop iterates through library array and adds the books to the dom
-for(let book of library){
-    let libCont = document.querySelector('.content');
-    let book1 = document.createElement(div);
-    book1.className = 'bookCard';
-    libCont.appendChild(book1);
 
-    let bookTitle = document.createElement(h4);
-    bookTitle.textContent ='Title: ' + this.name;
-    book1.appendChild(bookTitle);
+//Function that takes form input values
+function getVal(){
+    const titleVal = document.getElementById('title');
+    const authorVal = document.querySelector('#author');
+    const pagesVal = document.querySelector('#pages');
 
-    let bookAuthor = document.createElement(h4);
-    bookAuthor.textContent ='Author: ' + this.author;
-    book1.appendChild(bookAuthor);
-
-    let bookPages = document.createElement(h4);
-    bookPages.textContent = 'Pages: ' + this.pages;
-    book1.appendChild(bookPages);
+    const book = new addBook(titleVal.value, authorVal.value, pagesVal.value);
+    closeForm();
 }
 
+
+// Functions opens and closes the form.
 function openForm(){
     document.querySelector('.formCont').style.display = "grid";
   
@@ -39,6 +55,16 @@ function openForm(){
 
 function closeForm(){
     document.querySelector('.formCont').style.display = 'none';
+
+    //When form is closed it clears the inputs
+    const titleVal = document.getElementById('title');
+    const authorVal = document.querySelector('#author');
+    const pagesVal = document.querySelector('#pages');
+    const readVal = document.querySelector('read');
+    titleVal.value = '';
+    authorVal.value = '';
+    pagesVal.value = '';
+
 }
 
 
